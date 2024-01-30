b25dea00d3e36162e26f6e9f513583776e6f3ecc
ChantyTUN <cambodia4khmeru@gmail.com>
2024-01-30 14:44:18 +0700

    7

--- start2/lib/page/home.dart
+++ start2/lib/page/home.dart
@@ -11,17 +11,31 @@ class Home extends StatelessWidget {
       ),
       body: Column(
         children: [
-          Container(
-            width: double.infinity,
-            height: 220.0,
-            color: Colors.yellow,
-            child: Image(
-                image: AssetImage(
-                    "images/1.jpg"
+            Stack(
+              alignment: Alignment.center,
+              children: [
+                Container(
+                  width: double.infinity,
+                  height: 220.0,
+                  color: Colors.yellow,
+                  child: Image(
+                    image: AssetImage(
+                        "images/1.jpg"
+                    ),
+                    fit: BoxFit.cover,
+                  ),
                 ),
-              fit: BoxFit.cover,
-            ),
-          )
+                Container(
+                  padding: EdgeInsets.all(10.5),
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(8.0),
+                    color: Colors.white.withOpacity(0.5),
+                  ),
+                  child: Text("Welcome"),
+                )
+              ],
+            )
+
         ],
       )
     );
