dcfc7943a7d65e1eac33ea1add153b9cced342da
ChantyTUN <cambodia4khmeru@gmail.com>
2024-03-15 10:37:25 +0700

    27. future builder

--- start2/lib/page/page14.dart
+++ start2/lib/page/page14.dart
@@ -3,6 +3,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:http/http.dart' as http;
 import 'package:start2/api/source.dart';
+import 'package:start2/api/city.dart';
 
 class Page14 extends StatefulWidget {
   const Page14({super.key});
@@ -21,6 +22,26 @@ class _Page14State extends State<Page14> {
     return myData;
   }
 
+  Future<List<City>> getCity() async{
+    var url = "http://chey7.com/app/app-1/rean-web-admin/api/get-city-list.php?s=0&e=2";
+    List<City> myCity=[];
+      var response = await http.get(Uri.parse(url));
+      if(response.statusCode == 200){
+          var myData2 = jsonDecode(response.body);
+          for(var myitem in myData2){
+              myCity.add(
+                City(
+                    id: int.parse(myitem['id']),
+                    name: myitem['name'],
+                    des: myitem['des'],
+                    img: myitem['img']
+                )
+              );
+          }
+    }
+    return myCity;
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -28,7 +49,7 @@ class _Page14State extends State<Page14> {
         title: const Text("Pull to refresh"),
       ),
       body: FutureBuilder(
-        future: getData(),
+        future: getCity(),
         builder: (BuildContext context, AsyncSnapshot<List> snapshot){
           if(snapshot.hasData){
             return ListView.builder(
@@ -38,7 +59,7 @@ class _Page14State extends State<Page14> {
                     height: 130.0,
                     // color: Colors.blue,
                     child: Center(
-                      child: Text("${snapshot.data![ind]['name']}"),
+                      child: Text("${snapshot.data![ind].name}"),
                     ),
                   );
                 }
